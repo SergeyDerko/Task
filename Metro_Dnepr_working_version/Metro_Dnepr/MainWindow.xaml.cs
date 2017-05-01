99332bc7c426a541cc7fe818bcee5c27b4e2d481
SergeyDerko <sergeyderko1986@yandex.ru>
2017-05-02 00:08:00 +0300

    add Metro_Dnepr working wersion

---
+++ Metro_Dnepr_working_version/Metro_Dnepr/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Metro_Dnepr
+{
+
+    public partial class MainWindow
+    {
+        //Массив для выбора станций
+        private readonly Ellipse[] _mas;
+        //Массив всех элементов
+        private readonly Ellipse[] _masOll;
+        //Массив маленьких элипсов
+        private readonly Ellipse[] _masLite;
+        //выбранный маршрут
+        private List<Ellipse> _wey;
+        private int _count;
+        private bool _flag = true;
+        readonly DispatcherTimer _timer = new DispatcherTimer();
+        //начальная точка
+        private Ellipse _start;
+        //конечная точка
+        private Ellipse _stop;
+        
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mas = new[] { M1, M2, M3, M4, M5, M6 };
+            _masOll = new[] {M1,M11,M2,M21,M22,M3,M31,M33,M4,M41,M42,M5,M51,M52,M6};
+            _masLite = new[] {M11, M21, M22, M31, M33, M41, M42, M51, M52};
+            Addevent();
+            _timer.Tick += Test;
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+        }
+
+        private void Addevent()
+        {
+            foreach (var i in _mas)
+            {
+                i.MouseDown += delegate
+                {
+                    if (_start != null && _stop != null)
+                    {
+                        foreach (var j in _mas)
+                        {
+                            j.Fill = Brushes.Yellow;
+                        }
+                        foreach (var j in _masLite)
+                        {
+                            j.Fill = null;
+                        }
+                        _start = null;
+                        _stop = null;
+                        _count = 0;
+                    }
+                    i.Fill = Brushes.Aqua;
+                    if (!Equals(i, _start) && _start == null)
+                        _start = i;
+                    else
+                    if (!Equals(i, _stop))
+                        _stop = i;
+                    if (_start != null && _stop != null)
+                    {
+                        _flag = true;
+                        _wey = Run(_start, _stop);
+                        Timer(_flag);
+                    }
+
+                };
+            }
+        }
+        // выбор пути
+        private List<Ellipse> Run(Ellipse start, Ellipse stop)
+        {
+            var qvery = new List<Ellipse>();
+            Ellipse[] mas;
+            if (int.Parse(start.Tag.ToString()) < int.Parse(stop.Tag.ToString()))
+            {
+                mas = _masOll.OrderBy(x => Int16.Parse(x.Tag.ToString())).ToArray();
+                foreach (var i in mas)
+                {
+                    if (int.Parse(i.Tag.ToString()) > int.Parse(start.Tag.ToString()) &&
+                        int.Parse(i.Tag.ToString()) < int.Parse(stop.Tag.ToString()))
+                        qvery.Add(i);
+                }
+            }
+            if (int.Parse(start.Tag.ToString()) > int.Parse(stop.Tag.ToString()))
+            {
+                mas = _masOll.OrderByDescending(x => Int16.Parse(x.Tag.ToString())).ToArray();
+                foreach (var i in mas)
+                {
+                    if (int.Parse(i.Tag.ToString()) < int.Parse(start.Tag.ToString()) &&
+                        int.Parse(i.Tag.ToString()) > int.Parse(stop.Tag.ToString()))
+                        qvery.Add(i);
+                }
+            }
+            return qvery;
+        }
+
+        private void Timer(bool flag)
+        {
+            if (flag)
+            {
+                _timer.Start();
+                _flag = false;
+            }
+            else
+                _timer.Stop();
+        }
+
+        // прорисовка пути
+        private void Test(object sender, EventArgs e)
+        {
+            if (_count == _wey.Count)
+            {
+                _flag = false;
+                Timer(_flag);
+            }
+            while ( _count < _wey.Count)
+            {
+                _wey[_count].Fill = Brushes.Aqua;
+                _count++;
+                break;
+            }
+        }
+    }
+}
---
+++ Metro_Kiew_in_developing/Metro/Metro/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Metro
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        private int _count;
+        private bool _flag = true;
+        readonly DispatcherTimer _timer = new DispatcherTimer();
+        private Ellipse _start;
+        private Ellipse _stop;
+
+        readonly Ellipse[] _ollEllipses;
+            public MainWindow()
+        {
+            InitializeComponent();
+            _ollEllipses = new []
+            {
+                Red1,Red2,Red3,Red4,Red5,Red6,Red7,Red8,Red9,Red10,Red11,Red12,Red13,Red14,
+                Red15, Red16,Red17,Red18,Blue1,Blue2,Blue3,Blue4,Blue5,Blue6,Blue7,Blue8,
+                Blue9,Blue10,Blue11,Blue12,Blue13,Blue14,Blue15,Blue16,Blue17,Green1,Green2,Green3,
+                Green4,Green5,Green6,Green7,Green8,Green9,Green10,Green11,Green12,Green13,Green14
+            };
+
+            Addevent();
+            _timer.Tick += Test;
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+        }
+
+        private void Addevent()
+        {
+            foreach (var i in _ollEllipses)
+            {
+                i.MouseDown += delegate
+                {
+                    i.Fill = Brushes.Aqua;
+                    //    if(!Equals(i, _start))
+                    //        _start = i;
+                    //    if (!Equals(i, _stop))
+                    //        _stop = i;
+                    //    if (_start!= null && _stop!= null)
+                    //        Run(_start,_stop);
+                    //};
+                };
+            }
+        }
+
+        private void Run(Ellipse start, Ellipse stop)
+        {
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Timer(_flag);
+            for (int j = 0; j < _ollEllipses.Length; j++)
+            {
+                if (j < 18)
+                    _ollEllipses[j].Fill = Brushes.Red;
+                if (j >= 18 && j < 35)
+                    _ollEllipses[j].Fill = Brushes.Blue;
+                if (j >= 35)
+                    _ollEllipses[j].Fill = Brushes.Green;
+            }
+            _count = 0;
+            _flag = true;
+            Timer(_flag);
+            
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            for (int j = 0; j < _ollEllipses.Length; j++)
+            {
+                if (j < 18)
+                    _ollEllipses[j].Fill = Brushes.Red;
+                if (j >= 18 && j < 35)
+                    _ollEllipses[j].Fill = Brushes.Blue;
+                if (j >= 35)
+                    _ollEllipses[j].Fill = Brushes.Green;
+            }
+            _count = 0;
+            Timer(false);
+        }
+
+        
+
+
+
+         /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private void Timer( bool flag)
+        {
+
+            if (flag)
+            {
+                _timer.Start();
+                _flag = false;
+            }
+                
+            else
+                _timer.Stop();
+           
+        }
+
+       
+        private void Test(object sender, EventArgs e)
+        {
+            if (_count == _ollEllipses.Length)
+                _flag = false;
+            while (_count<_ollEllipses.Length)
+            {
+                _ollEllipses[_count].Fill = Brushes.Yellow;
+                _count++;
+                break;
+            }
+           
+        }
+
+        
+    }
+}
